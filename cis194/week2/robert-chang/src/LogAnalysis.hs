683385ae2ca14117c1c1c5ae604cf25868ad7585
Robert Chang <cht8687@gmail.com>
2019-08-15 08:03:38 +1000

    Week2-Task1

---
+++ cis194/week2/robert-chang/src/LogAnalysis.hs
@@ -0,0 +1,39 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module LogAnalysis where
+import Data.Maybe (isJust, fromJust)
+import Text.Read ( readMaybe )
+import Log
+
+-- Exercise 1
+
+-- Helpers
+safeIntReader :: String -> Maybe Int
+safeIntReader = readMaybe
+
+getIntFromJust :: String -> Int 
+getIntFromJust s = fromJust . safeIntReader $ s 
+
+isJustValue :: String -> Bool
+isJustValue s = isJust . safeIntReader $ s
+
+parseMessage :: String -> LogMessage
+parseMessage s = case words s of 
+             ("E": level : timestamp : msg)  -> 
+               if (and [isJustValue level, isJustValue timestamp])
+                  then LogMessage (Error $ getIntFromJust $ level) (getIntFromJust timestamp) (unwords msg)
+                  else Unknown s
+             ("I": timestamp : msg) ->
+               if (isJustValue timestamp)
+                  then LogMessage Info (getIntFromJust timestamp) (unwords msg)
+                  else Unknown s
+             ("W": timestamp : msg) ->
+               if (isJustValue timestamp)
+                  then LogMessage Warning (getIntFromJust timestamp) (unwords msg)
+                  else Unknown s
+             __ -> Unknown s
+
+parse :: String -> [LogMessage]
+parse [] = []
+parse file = map parseMessage $ lines file
+

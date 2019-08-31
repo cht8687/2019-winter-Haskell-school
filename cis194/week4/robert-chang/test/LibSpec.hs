b5c81169e8dd6ae5831c9dee00b6a25939438bff
Robert Chang <cht8687@gmail.com>
2019-08-31 23:59:18 +1000

    [Week4]:Task1]

---
+++ cis194/week4/robert-chang/test/LibSpec.hs
@@ -0,0 +1,28 @@
+module LibSpec where
+import Lib
+import Test.Hspec
+
+spec :: Spec
+spec =
+  describe "Week4" $ do
+   wholemealSpec
+
+wholemealSpec :: Spec
+wholemealSpec = do
+  it "returns 1 for an empty list" $
+    fun1' [] `shouldBe` fun1 [] 
+
+  it "returns the product even numbers subtracting 2" $
+    fun1' [4, 5, 6, 7, 8, 9, 10] `shouldBe` fun1 [4, 5, 6, 7, 8, 9, 10]
+
+  it "case 1" $
+    fun2' 1 `shouldBe` fun2 1 
+
+  it "case 2" $
+    fun2' 2 `shouldBe` fun2 2 
+
+  it "case 3" $
+    fun2' 3 `shouldBe` fun2 3 
+
+  it "case 100" $
+    fun2' 100 `shouldBe` fun2 100

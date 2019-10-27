ae65580b3c8b2400bf24594c14a24b7c997e78e3
Robert Chang <cht8687@gmail.com>
2019-10-27 20:18:54 +1100

    (week11): 1-3

---
+++ cis194/week11/haotian/src/SExpr.hs
@@ -0,0 +1,60 @@
+{- CIS 194 HW 11
+   due Monday, 8 April
+-}
+
+module SExpr where
+
+import AParser
+import Control.Applicative
+import Data.Char
+
+------------------------------------------------------------
+--  1. Parsing repetitions
+------------------------------------------------------------
+
+zeroOrMore :: Parser a -> Parser [a]
+zeroOrMore p = oneOrMore p <|> pure []
+
+oneOrMore :: Parser a -> Parser [a]
+oneOrMore p = (:) <$> p <*> zeroOrMore p
+
+------------------------------------------------------------
+--  2. Utilities
+------------------------------------------------------------
+
+spaces :: Parser String
+spaces = (:) <$> satisfy isSpace <*> spaces <|> pure ""
+
+ident :: Parser String
+ident = (:) <$> satisfy isAlpha <*> zeroOrMore (satisfy isAlphaNum)
+
+------------------------------------------------------------
+--  3. Parsing S-expressions
+------------------------------------------------------------
+
+-- An "identifier" is represented as just a String; however, only
+-- those Strings consisting of a letter followed by any number of
+-- letters and digits are valid identifiers.
+type Ident = String
+
+-- An "atom" is either an integer value or an identifier.
+data Atom = N Integer | I Ident
+  deriving Show
+
+-- An S-expression is either an atom, or a list of S-expressions.
+data SExpr = A Atom
+           | Comb [SExpr]
+  deriving Show
+
+parseAtom :: Parser Atom
+parseAtom = (I <$> ident) <|> (N <$> posInt)
+
+withSpaces :: Parser a -> Parser a
+withSpaces p = spaces *> p <* spaces
+
+withParens :: Parser a -> Parser a
+withParens p = char '(' *> p <* char ')'
+
+parseSExpr :: Parser SExpr
+parseSExpr = withSpaces $
+  (A <$> parseAtom) <|> withParens (Comb <$> oneOrMore parseSExpr)
